dba9e74d540e1800efde335944402495b46a165b
Sumedh Suralkar <72144790+sumedhx@users.noreply.github.com>
2023-07-13 13:27:48 +0530

    Delete Level 2 - Task 3 - (TemperatureConverter) directory

--- Level 2 - Task 3 - (TemperatureConverter)/index.js
+++
@@ -1,5 +0,0 @@
-function temperatureConverter(valNum) {
-    valNum = parseFloat(valNum);
-    document.getElementById("outputFahrenheit").innerHTML=(valNum*1.8)+32;
-    document.getElementById("outputKelvin").innerHTML=valNum+273.15;
-  }
